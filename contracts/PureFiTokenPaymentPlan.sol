05e652482c51e0233d25c900e45f12784a06511a
Mykhailo Tiutin <miha.tiutin@purefi.io>
2021-07-08 15:39:35 +0300

    Added Payment Plan contract

---
+++ contracts/PureFiTokenPaymentPlan.sol
@@ -0,0 +1,125 @@
+pragma solidity ^0.8.6;
+
+
+import "../openzeppelin-contracts-master/contracts/token/ERC20/IERC20.sol";
+import "../openzeppelin-contracts-master/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../openzeppelin-contracts-master/contracts/utils/math/SafeMath.sol";
+import "../openzeppelin-contracts-master/contracts/access/Ownable.sol";
+
+
+contract PureFiTokenPaymentPlan is Ownable {
+
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  struct PaymentPlan{
+    uint64[] unlockDateShift; //timestamp shift of the unlock date; I.e. unlock date = unlockDateShift + startDate
+    uint16[] unlockPercent; //unlock percents multiplied by 100;
+  }
+
+  struct Vesting{
+    uint8 paymentPlan; //payment plan ID
+    uint64 startDate; //payment plan initiation date. Can be 0 if PaymentPlan refers to exact unlock timestamps.
+    uint256 totalAmount; //total amount of tokens vested for a person
+    uint256 withdrawnAmount; //amount withdrawn by user
+  }
+
+  PaymentPlan[] internal paymentPlans;
+  mapping (address => Vesting) internal vestedTokens;
+
+  IERC20 internal token;
+  uint64 public startDate; //timestamp this contracts start operations at. Claiming tokens is not available before this date
+  uint256 public totalVestedAmount; // total amount of vested tokens under this contract control.
+
+  event Withdrawal(address indexed who, uint256 amount);
+  event PaymentPlanAdded(uint256 index);
+  event TokensVested(address indexed beneficiary, uint8 paymentPlan, uint64 startDate, uint256 amount);
+  
+  constructor (
+    address _token,
+    uint64 _start
+  )
+  {
+    require(_token != address(0),"incorrect token address");
+    require(_start > block.timestamp, "incorrect start time");
+    token = IERC20(_token);
+    startDate = _start;
+  }
+
+
+  function addPaymentPlan(uint64[] memory _ts, uint16[] memory _percents) public onlyOwner(){
+    require(_ts.length == _percents.length,"array length doesn't match");
+    uint16 totalPercent = 0;
+    uint16 prevPercent = 0;
+    uint64 prevDate = 0;
+    for(uint i = 0; i < _ts.length; i++){
+      require (prevPercent <= _percents[i], "Incorrect percent value");
+      require (prevDate <= _ts[i], "Incorrect unlock date value");
+      prevPercent = _percents[i];
+      prevDate = _ts[i];
+      totalPercent += _percents[i];
+    }
+    require(totalPercent == 10000, "Total percent is not 100%");
+    
+    paymentPlans.push(PaymentPlan(_ts, _percents));
+    emit PaymentPlanAdded(paymentPlans.length - 1);
+  }
+
+  function vestTokens(uint8 _paymentPlan, uint64 _startDate, uint256 _amount, address _beneficiary) public onlyOwner {
+    require(vestedTokens[_beneficiary].totalAmount == 0, "This address already has vested tokens");
+    require(paymentPlans.length > _paymentPlan, "Incorrect payment plan index");
+    require(_amount > 0, "Can't vest 0 tokens");
+    require(token.balanceOf(address(this)) >= totalVestedAmount + _amount, "Not enough tokens for this vesting plan");
+    vestedTokens[_beneficiary] = Vesting(_paymentPlan, _startDate, _amount, 0);
+    totalVestedAmount += _amount;
+    emit TokensVested(_beneficiary, _paymentPlan, _startDate, _amount);
+  }
+
+  function withdraw(uint256 _amount) public {
+    require(vestedTokens[msg.sender].totalAmount > 0,"No tokens vested for this address");
+    (, uint256 available) = withdrawableAmount(msg.sender);
+    require(_amount <= available, "Amount exeeded current withdrawable amount");
+    require(available > 0, "Nothing to withdraw");
+    vestedTokens[msg.sender].withdrawnAmount += _amount;
+    token.safeTransfer(msg.sender, _amount);
+    emit Withdrawal(msg.sender, _amount);
+  }
+
+  function withdrawableAmount(address _beneficiary) public view returns (uint64, uint256) {
+    require(vestedTokens[_beneficiary].totalAmount > 0,"No tokens vested for this address");
+    uint16 percentLocked = 0;
+    uint64 paymentPlanStartDate = vestedTokens[_beneficiary].startDate;
+    uint256 index = paymentPlans[vestedTokens[_beneficiary].paymentPlan].unlockPercent.length;
+    uint64 nextUnlockDate = 0;
+    while (index > 0 && uint64(block.timestamp) < paymentPlanStartDate + paymentPlans[vestedTokens[_beneficiary].paymentPlan].unlockDateShift[index-1]) {
+      index--;
+      nextUnlockDate = paymentPlanStartDate + paymentPlans[vestedTokens[_beneficiary].paymentPlan].unlockDateShift[index];
+      percentLocked += paymentPlans[vestedTokens[_beneficiary].paymentPlan].unlockPercent[index];
+      
+    }
+    uint256 amountLocked = vestedTokens[_beneficiary].totalAmount*percentLocked/10000;
+    uint256 remaining = vestedTokens[_beneficiary].totalAmount - vestedTokens[_beneficiary].withdrawnAmount;
+    uint256 available = 0;
+    if (remaining > amountLocked){
+      available = remaining - amountLocked;
+    } else {
+      //overflow
+      available = 0;
+    }
+
+    return (nextUnlockDate, available);
+  }
+
+  function vestingData(address _beneficiary) public view returns (uint8, uint64, uint64, uint256, uint256, uint256) {
+    (uint64 nextUnlockDate, uint256 available) = withdrawableAmount(_beneficiary);
+    return (vestedTokens[_beneficiary].paymentPlan, vestedTokens[_beneficiary].startDate, nextUnlockDate, vestedTokens[_beneficiary].totalAmount, vestedTokens[_beneficiary].withdrawnAmount, available);
+  }
+
+  function paymentPlanLength(uint256 _paymentPlan) public view returns(uint256){
+    return paymentPlans[_paymentPlan].unlockPercent.length;
+  }
+
+  function paymentPlanData(uint256 _paymentPlan, uint256 _index) public view returns (uint64, uint16){
+    return (paymentPlans[_paymentPlan].unlockDateShift[_index], paymentPlans[_paymentPlan].unlockPercent[_index]);
+  }
+}
--- test/purefiTokenTest.js
+++ test/purefiTokenTest.js
@@ -1,4 +1,4 @@
-
+const { time, expectRevert } = require('openzeppelin-test-helpers');
 const bigDecimal = require('js-big-decimal');
 const web3 = require("web3");
 const BN = web3.utils.BN;
@@ -10,10 +10,25 @@ chai.use(require('chai-match'));
 
 
 
+
 const PureFiToken = artifacts.require('PureFiToken');
+const PureFiTokenPaymentPlan = artifacts.require('PureFiTokenPaymentPlan');
 
 function toBN(number) {
     return web3.utils.toBN(number);
+}
+
+function printEvents(txResult, strdata){
+    console.log(strdata," events:",txResult.logs.length);
+    for(var i=0;i<txResult.logs.length;i++){
+        let argsLength = Object.keys(txResult.logs[i].args).length;
+        console.log("Event ",txResult.logs[i].event, "  length:",argsLength);
+        for(var j=0;j<argsLength;j++){
+            if(!(typeof txResult.logs[i].args[j] === 'undefined') && txResult.logs[i].args[j].toString().length>0)
+                console.log(">",i,">",j," ",txResult.logs[i].args[j].toString());
+        }
+    }
+
 }
 
 
@@ -25,17 +40,159 @@ contract('PureFiToken', (accounts) => {
     console.log("Test: Admin: "+admin);
 
     let pureFiToken;
+    let paymentPlan;
+    // const startDate = 1627383600; // Jul 27 11:00 UTC
+    const startDate = Math.round((new Date().getTime())/1000)+10;
 
 
     before(async () => {
         await PureFiToken.deployed().then(instance => pureFiToken = instance);
+        console.log("startDate=",startDate);
+        await PureFiTokenPaymentPlan.new(pureFiToken.address, toBN(startDate)).then(instance => paymentPlan = instance);
+
+        
     });
 
     it('PureFi Token', async () => {
 
         let balance = await pureFiToken.balanceOf.call(admin);
+        console.log("balance",balance.toString());
         expect(balance).to.be.eq.BN(toBN(100000000).mul(decimals));
   
     });
+
+    it('add payment plan', async () => {
+
+        let tgePercent = 33;
+        let cliff = 1; //month
+        let payout = 33.5;
+        let payoutTimes = 2; //month
+
+        const monthPeriod = 60*60*24*30; //30 days
+        let unlockDates = [toBN(0)];
+        let unlockPercents = [toBN(Math.round(tgePercent*100))];
+        for( let i =0;i<payoutTimes;i++){
+            unlockDates.push(toBN((cliff+i+1)*monthPeriod));
+            unlockPercents.push(toBN(Math.round(payout*100)));
+        }
+        console.log("Adding payment plan:");
+        for( let i =0;i<unlockDates.length;i++){
+            console.log(i, ": ",unlockDates[i].toString()," - ",unlockPercents[i].toString());
+        }
+
+        let addPlanRes = await paymentPlan.addPaymentPlan.sendTransaction(unlockDates,unlockPercents);
+        printEvents(addPlanRes, "Add payment plan");
+  
+    });
+
+    it('add Vested Tokens', async () => {
+        await pureFiToken.transfer.sendTransaction(paymentPlan.address,toBN(100000).mul(decimals));
+        // vestTokens(uint8 _paymentPlan, uint64 _startDate, uint256 _amount, address _beneficiary)
+        let totalVested = toBN(100).mul(decimals);
+        let addVest = await paymentPlan.vestTokens.sendTransaction(toBN(0),startDate,totalVested,accounts[0]);
+        printEvents(addVest, "addVest");
+    
+        //before start
+        {
+            let withdrawable = await paymentPlan.withdrawableAmount.call(accounts[0]);
+            let nextUnlockDate = withdrawable[0].toNumber();
+            let availableAmount = withdrawable[1];
+            console.log(nextUnlockDate," ", availableAmount.toString());
+            expect(availableAmount).to.be.eq.BN(toBN(0));
+
+            await expectRevert(
+                paymentPlan.withdraw(totalVested, {from: accounts[0]}),
+                'revert'
+            );
+        }
+        //just after start (TGE payment)
+        await time.increase(time.duration.days(1));
+        {
+            let withdrawable = await paymentPlan.withdrawableAmount.call(accounts[0]);
+            let nextUnlockDate = withdrawable[0].toNumber();
+            let availableAmount = withdrawable[1];
+            console.log(nextUnlockDate," ", availableAmount.toString());
+            expect(availableAmount).to.be.eq.BN(toBN('33000000000000000000'));
+            //attempt withdraw full amount
+            await expectRevert(
+                paymentPlan.withdraw(totalVested, {from: accounts[0]}),
+                'revert'
+            );
+            
+            {
+                let balanceBefore = await pureFiToken.balanceOf(accounts[0]);
+                await paymentPlan.withdraw(availableAmount, {from: accounts[0]})
+                let balanceAfter = await pureFiToken.balanceOf(accounts[0]);
+                expect(balanceAfter.sub(balanceBefore)).to.be.eq.BN(availableAmount);
+            }
+        }
+
+
+        //1 month (cliff, no payment)
+        await time.increase(time.duration.days(30));
+        {
+            let withdrawable = await paymentPlan.withdrawableAmount.call(accounts[0]);
+            let nextUnlockDate = withdrawable[0].toNumber();
+            let availableAmount = withdrawable[1];
+            console.log(nextUnlockDate," ", availableAmount.toString());
+            expect(availableAmount).to.be.eq.BN(toBN('0'));
+        }
+        //2 month (1st payout)
+        await time.increase(time.duration.days(30));
+        {
+            let withdrawable = await paymentPlan.withdrawableAmount.call(accounts[0]);
+            let nextUnlockDate = withdrawable[0].toNumber();
+            let availableAmount = withdrawable[1];
+            console.log(nextUnlockDate," ", availableAmount.toString());
+            expect(availableAmount).to.be.eq.BN(toBN('33500000000000000000'));
+
+            await expectRevert(
+                paymentPlan.withdraw(totalVested, {from: accounts[0]}),
+                'revert'
+            );
+            
+            {
+                let balanceBefore = await pureFiToken.balanceOf(accounts[0]);
+                await paymentPlan.withdraw(availableAmount, {from: accounts[0]})
+                let balanceAfter = await pureFiToken.balanceOf(accounts[0]);
+                expect(balanceAfter.sub(balanceBefore)).to.be.eq.BN(availableAmount);
+            }
+        }
+        //3 month (2nd payment - full amount)
+        await time.increase(time.duration.days(30));
+        {
+            let withdrawable = await paymentPlan.withdrawableAmount.call(accounts[0]);
+            let nextUnlockDate = withdrawable[0].toNumber();
+            let availableAmount = withdrawable[1];
+            console.log(nextUnlockDate," ", availableAmount.toString());
+            expect(availableAmount).to.be.eq.BN(toBN('33500000000000000000'));
+
+            await expectRevert(
+                paymentPlan.withdraw(totalVested, {from: accounts[0]}),
+                'revert'
+            );
+            
+            {
+                let balanceBefore = await pureFiToken.balanceOf(accounts[0]);
+                await paymentPlan.withdraw(availableAmount, {from: accounts[0]})
+                let balanceAfter = await pureFiToken.balanceOf(accounts[0]);
+                expect(balanceAfter.sub(balanceBefore)).to.be.eq.BN(availableAmount);
+            }
+        }
+
+        {
+            let withdrawable = await paymentPlan.withdrawableAmount.call(accounts[0]);
+            let nextUnlockDate = withdrawable[0].toNumber();
+            let availableAmount = withdrawable[1];
+            console.log(nextUnlockDate," ", availableAmount.toString());
+            expect(availableAmount).to.be.eq.BN(toBN('0'));
+        }
+
+
+  
+    });
+
+
+    
    
 });
