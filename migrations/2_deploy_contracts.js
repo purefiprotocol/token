ebc74c7aa68834aadccb2031d950cae38602da09
Mykhailo Tiutin <miha.tiutin@purefi.io>
2021-07-23 17:20:09 +0300

    fixed migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -16,30 +16,30 @@ function toBN(number) {
 module.exports = async function (deployer, network, accounts) {
     
     let admin = accounts[0];
-    let trustedForwarder;
+    // let trustedForwarder;
 
-    if(network == 'rinkeby' || network == 'rinkeby-fork'){
-        trustedForwarder = '0x83A54884bE4657706785D7309cf46B58FE5f6e8a';
-    } else if(network == 'mainnet' || network == 'mainnet-fork'){
-        trustedForwarder = '0xAa3E82b4c4093b4bA13Cb5714382C99ADBf750cA';
-    } else if(network == 'kovan' || network == 'kovan-fork'){
-        trustedForwarder = '0x7eEae829DF28F9Ce522274D5771A6Be91d00E5ED';
-    }else if(network == 'ropsten' || network == 'ropsten-fork'){
-        trustedForwarder = '0xeB230bF62267E94e657b5cbE74bdcea78EB3a5AB';
-    }else if(network == 'test'){
-        trustedForwarder = '0xAa3E82b4c4093b4bA13Cb5714382C99ADBf750cA';
-    }else if(network == 'bsctest' || network == 'bsctest-fork'){
-        trustedForwarder = '0xeB230bF62267E94e657b5cbE74bdcea78EB3a5AB';
-    }else if(network == 'bsc' || network == 'bsc-fork'){
-        trustedForwarder = '0xeB230bF62267E94e657b5cbE74bdcea78EB3a5AB';
+    // if(network == 'rinkeby' || network == 'rinkeby-fork'){
+    //     trustedForwarder = '0x83A54884bE4657706785D7309cf46B58FE5f6e8a';
+    // } else if(network == 'mainnet' || network == 'mainnet-fork'){
+    //     trustedForwarder = '0xAa3E82b4c4093b4bA13Cb5714382C99ADBf750cA';
+    // } else if(network == 'kovan' || network == 'kovan-fork'){
+    //     trustedForwarder = '0x7eEae829DF28F9Ce522274D5771A6Be91d00E5ED';
+    // }else if(network == 'ropsten' || network == 'ropsten-fork'){
+    //     trustedForwarder = '0xeB230bF62267E94e657b5cbE74bdcea78EB3a5AB';
+    // }else if(network == 'test'){
+    //     trustedForwarder = '0xAa3E82b4c4093b4bA13Cb5714382C99ADBf750cA';
+    // }else if(network == 'bsctest' || network == 'bsctest-fork'){
+    //     trustedForwarder = '0xeB230bF62267E94e657b5cbE74bdcea78EB3a5AB';
+    // }else if(network == 'bsc' || network == 'bsc-fork'){
+    //     trustedForwarder = '0xeB230bF62267E94e657b5cbE74bdcea78EB3a5AB';
     }
 
     console.log("Deploy: Admin: "+admin);
-    console.log("Deploy: forwarder: "+trustedForwarder);
+    // console.log("Deploy: forwarder: "+trustedForwarder);
 
     
     let pureFiToken;
-    await deployer.deploy(PureFiToken, admin, trustedForwarder)
+    await deployer.deploy(PureFiToken, admin)
     .then(function(){
         console.log("PureFiToken instance: ", PureFiToken.address);
         return PureFiToken.at(PureFiToken.address);
